47b287c8af119643a4671fe33eb343a989758eab
Ethan <project.ethang@gmail.com>
2026-01-12 14:18:33 -0500

    Added SQL functionality, storing potential inputs to the neural network in a database

---
+++ database.py
@@ -0,0 +1,72 @@
+#import iit_computation
+#import tpm_generator
+import sqlite3
+import json
+
+# Store the following in a db: MI, size of tpm, prior, max mi, max part
+
+con = sqlite3.connect("test.db")
+cur = con.cursor()
+
+def create_db():
+    cur.execute('''CREATE TABLE IF NOT EXISTS network_property
+                   (
+                       idx
+                       INTEGER
+                       PRIMARY
+                       KEY
+                       AUTOINCREMENT,
+                       ii
+                       REAL,
+                       mi
+                       REAL,
+                       max_bipartition
+                       TEXT,
+                       max_mi
+                       REAL,
+                       num_nodes
+                       INTEGER
+                   )
+                ''')
+
+def close_db():
+    con.close()
+
+def drop_db():
+    cur.execute('''DROP TABLE IF EXISTS network_property''')
+
+def write_to_db(network_properties: list[tuple[float, float, tuple[list[int]] | None, float, int]]) -> None:
+    network_properties_db: list[tuple[float, float, str, float, int]] = []
+    for property in network_properties:
+        ii, mi_Xt_Xtpast, max_bipartition, max_mi, num_nodes = property
+        max_bipartition = json.dumps(max_bipartition)
+        network_properties_db.append((ii, mi_Xt_Xtpast, max_bipartition, max_mi, num_nodes))
+
+    with con:
+        cur.executemany('''
+                        INSERT INTO network_property (ii,
+                mi,
+                max_bipartition,
+               max_mi,
+               num_nodes)
+                        VALUES (?, ?, ?, ?, ?)''', network_properties_db)
+        con.commit()
+
+
+def get_row_by_idx(idx: int) -> tuple[float, float, tuple[list[int]] | None, float, int] | None:
+    with con:
+        res = cur.execute('''SELECT ii, mi, max_bipartition, max_mi, num_nodes FROM network_property WHERE idx = ?''', [idx])
+        row = res.fetchone()
+
+        if row is None:
+            return None
+
+        ii, mi_Xt_Xtpast, max_bipartition, max_mi, num_nodes = row
+        max_bipartition = json.loads(max_bipartition)
+        return ii, mi_Xt_Xtpast, max_bipartition, max_mi, num_nodes
+
+# Next goal: databasing. A large number of quantities from computing integrated info
+# are going to need to be considered as estimators for the integrated information, so
+# across a large number of simulated tpms we will have a lot to store. There are two main
+# ways to do this; either storing in a file, or in a sql database
+
--- iit_three_node.py
+++ iit_three_node.py
@@ -1,9 +1,11 @@
 from tpm_generator import generate_det_tpm
 import iit_computation
 import numpy as np
+from database import write_to_db, get_row_by_idx, close_db, drop_db, create_db
 
 # Example usage for a 3 node systems with uni prior and random tpms
-tpm = generate_det_tpm(2**3)
+num_nodes: int = 3
+tpm = generate_det_tpm(2**num_nodes)
 prior = iit_computation.uniform_prior(tpm)
 
 output = iit_computation.integrated_information(tpm, prior)
@@ -13,18 +15,30 @@ print("\nIntegrated information: ", output[0], "\n",
       "Maximum Mutual Information across partitions:", output[3])
 
 # Example usage for multiple 3 node systems
-num_tpms = 3
-n_states = 2**3
+num_tpms = 20
+n_states = 2**4
 shape = (n_states, n_states)
 
 tpm = np.zeros((num_tpms, *shape), dtype=int)
 prior = np.zeros((num_tpms, n_states), dtype=float)
 
+# Stores ii, mi_Xt_Xtpast, max_bipartition, max_mi, num_nodes
+network_properties: list[tuple[float, float, tuple[list[int]] | None, float, int]] = []
+
 for i in range(num_tpms):
-    tpm[i] = generate_det_tpm(2**3)
+    tpm[i] = generate_det_tpm(2**4)
     prior[i] = iit_computation.uniform_prior(tpm[i])
 
-    # Compute and write to file
-    iit_computation.integrated_information(tpm[i], prior[i])
+    # Append to list for db data entry
+    ii, mi_Xt_Xtpast, max_bipartition, max_mi = iit_computation.integrated_information(tpm[i], prior[i])
+    network_properties.append((ii, mi_Xt_Xtpast, max_bipartition, max_mi, num_nodes))
 
+drop_db()
+create_db()
 
+write_to_db(network_properties)
+
+# Example row
+print(get_row_by_idx(20))
+
+close_db()
