340f5ad4290931d5df3f4694323bf58b32750ff5
Ethan <project.ethang@gmail.com>
2026-03-01 17:11:37 -0500

    Added the prior to the database, serving as another predictor for the neural network to train off of.

--- database.py
+++ database.py
@@ -2,11 +2,21 @@
 #import tpm_generator
 import sqlite3
 import json
+import numpy as np
+import numpy.typing as npt
+
 
 # Store the following in a db: MI, size of tpm, prior, max mi, max part
 
 con = sqlite3.connect("test.db")
 cur = con.cursor()
+
+# Properly loading the
+def deserialize_prior(text: str) -> npt.NDArray[np.float64]:
+    return np.array(json.loads(text), dtype=np.float64)
+
+def deserialize_bipartition(text: str) -> tuple[list[int]]:
+    return tuple(json.loads(text))
 
 def create_db():
     cur.execute('''CREATE TABLE IF NOT EXISTS network_property
@@ -25,7 +35,9 @@ def create_db():
                        max_mi
                        REAL,
                        num_nodes
-                       INTEGER
+                       INTEGER,
+                       tpm_prior
+                       TEXT
                    )
                 ''')
 
@@ -35,12 +47,14 @@ def close_db():
 def drop_db():
     cur.execute('''DROP TABLE IF EXISTS network_property''')
 
-def write_to_db(network_properties: list[tuple[float, float, tuple[list[int]] | None, float, int]]) -> None:
-    network_properties_db: list[tuple[float, float, str, float, int]] = []
+def write_to_db(network_properties: list[tuple[float, float, tuple[list[int]] | None, float, int, npt.NDArray[np.float64]]]) -> None:
+    network_properties_db: list[tuple[float, float, str, float, int, str]] = []
     for property in network_properties:
-        ii, mi_Xt_Xtpast, max_bipartition, max_mi, num_nodes = property
+        ii, mi_Xt_Xtpast, max_bipartition, max_mi, num_nodes, tpm_prior = property
         max_bipartition = json.dumps(max_bipartition)
-        network_properties_db.append((ii, mi_Xt_Xtpast, max_bipartition, max_mi, num_nodes))
+        # JSON serialization does not work with non-native (i.e; NDArray) datatype so we need to cast to a list first
+        tpm_prior = json.dumps(tpm_prior.tolist())
+        network_properties_db.append((ii, mi_Xt_Xtpast, max_bipartition, max_mi, num_nodes, tpm_prior))
 
     with con:
         cur.executemany('''
@@ -48,24 +62,66 @@ def write_to_db(network_properties: list[tuple[float, float, tuple[list[int]] | 
                 mi,
                 max_bipartition,
                max_mi,
-               num_nodes)
-                        VALUES (?, ?, ?, ?, ?)''', network_properties_db)
+               num_nodes,
+                tpm_prior)
+                        VALUES (?, ?, ?, ?, ?, ?)''', network_properties_db)
         con.commit()
 
 
-def get_row_by_idx(idx: int) -> tuple[float, float, tuple[list[int]] | None, float, int] | None:
+def get_row_by_idx(idx: int) -> tuple[float, float, tuple[list[int]] | None, float, int, npt.NDArray[np.float64]] | None:
+
     with con:
-        res = cur.execute('''SELECT ii, mi, max_bipartition, max_mi, num_nodes FROM network_property WHERE idx = ?''', [idx])
+        res = cur.execute(
+            '''
+            SELECT ii, mi, max_bipartition, max_mi, num_nodes, tpm_prior
+            FROM network_property
+            WHERE idx = ?
+            ''',
+            [idx]
+        )
+
         row = res.fetchone()
 
         if row is None:
             return None
 
-        ii, mi_Xt_Xtpast, max_bipartition, max_mi, num_nodes = row
-        max_bipartition = json.loads(max_bipartition)
-        return ii, mi_Xt_Xtpast, max_bipartition, max_mi, num_nodes
+        ii, mi_Xt_Xtpast, max_bipartition, max_mi, num_nodes, tpm_prior = row
 
-def get_all_rows():
+        # Deserialize MIP ONLY when it is an actual list (otherwise return None as it is stored in DB)
+        max_bipartition = (
+            deserialize_bipartition(max_bipartition)
+            if max_bipartition is not None
+            else None
+        )
+
+        # Deserialize prior
+        if tpm_prior is None:
+            raise ValueError("tpm_prior is NULL in database")
+        tpm_prior = deserialize_prior(tpm_prior)
+
+        return ii, mi_Xt_Xtpast, max_bipartition, max_mi, num_nodes, tpm_prior
+
+
+def get_all_rows() -> list[dict]:
     cur.execute("SELECT * FROM network_property")
     rows = cur.fetchall()
-    return rows
+
+    # Still need to deserialize the json dumped predictors (MIP and prior)
+    col_names = [desc[0] for desc in cur.description]
+
+    processed_rows = []
+
+    for row in rows:
+        row_dict = dict(zip(col_names, row))
+
+        # Deserialize prior
+        if row_dict.get("tpm_prior") is not None:
+            row_dict["tpm_prior"] = deserialize_prior(row_dict["tpm_prior"])
+
+        # Deserialize MIP
+        if row_dict.get("max_bipartition") is not None:
+            row_dict["max_bipartition"] = deserialize_bipartition(row_dict["max_bipartition"])
+
+        processed_rows.append(row_dict)
+
+    return processed_rows
